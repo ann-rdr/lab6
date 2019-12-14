33da9970949a43f6f07ffa190d3a3c8dbf943ef1
ann-rdr <55615125+ann-rdr@users.noreply.github.com>
2019-12-14 09:25:29 +0300

    Add files via upload

---
+++ lab6/src/Testik.java
@@ -0,0 +1,10 @@
+public class Testik {
+        public static void main(String[] args) {
+            Car c1 = new Car("Mercedez", "378bd", "GT", 247842, 2019);
+            Car c2 = new Car("Lada", "Granta", "Cross", 40000, 1999);
+            Car c3 = new Car("Shkoda", "JX", "Standart", 230000, 1978);
+            c1.println();
+            c2.println();
+            c3.println();
+        }
+    }
